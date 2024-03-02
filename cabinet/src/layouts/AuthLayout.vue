ed1af7a5f65c17ddf92e12f0e76ba80f5d5d0248
guzuro <guzurogmn@gmail.com>
2024-03-02 22:30:44 +0300

    client: add authlayout

---
+++ cabinet/src/layouts/AuthLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="auth-layout-container container">
+    <div class="auth-layout">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<style scoped lang="scss">
+.auth-layout {
+  &-container {
+    min-height: inherit;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  max-width: 695px;
+  width: 500px;
+}
+</style>
