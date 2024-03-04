926c9c4372a63d8601695581eb1b5560765d78b2
guzuro <guzurogmn@gmail.com>
2024-03-04 23:34:35 +0300

    server: started routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,21 @@
 import { fastify, FastifyInstance } from 'fastify'
+import autoLoad from '@fastify/autoload'
+import path from 'path';
+import { fileURLToPath } from 'url'
 
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 const server: FastifyInstance = fastify()
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+server.register(autoLoad, {
+  dir: path.join(dirname, 'routes'),
+  options: {
+    prefix: '/api'
+  }
+});
+
+// server.get('/ping', async (request, reply) => {
+//   return 'pong\n'
+// })
 
 server.listen({ port: 8080 }, () => {
   console.log('running')
