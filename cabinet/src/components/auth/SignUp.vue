f6c3ae3b8b900e7ee3de0066627c591cae1f57e6
guzuro <guzurogmn@gmail.com>
2024-03-04 23:36:00 +0300

    client: updated SignUp.vue form

--- cabinet/src/components/auth/SignUp.vue
+++ cabinet/src/components/auth/SignUp.vue
@@ -1,7 +1,73 @@
 <template>
-  <div class="sign-up">111</div>
+  <va-card class="sign-in">
+    <va-card-title>Sign up</va-card-title>
+
+    <va-card-content>
+      <VaForm
+        ref="form"
+        class="flex flex-column gap-2"
+      >
+        <VaInput
+          v-model="formFields.username"
+          :rules="[(value) => requiredInput(value, 'Username')]"
+          label="Username"
+        />
+
+        <VaInput
+          v-model="formFields.password"
+          type="password"
+          :rules="[(value) => requiredInput(value, 'Password')]"
+          label="Password"
+        />
+
+        <VaButton
+          :disabled="!isValid"
+          @click="validate() && submit()"
+        >
+          Sign up
+        </VaButton>
+      </VaForm>
+
+      <div class="mt-3 va-text-center">
+        <div>
+          Already has account?
+          <a
+            class="va-link va-text-bold"
+            @click.prevent="gotoSignUp"
+          >
+            Sign In
+          </a>
+        </div>
+      </div>
+    </va-card-content>
+  </va-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+import { VaCard, VaCardTitle, VaCardContent, VaForm, VaInput, VaButton, useForm } from "vuestic-ui";
+import { useRouter } from "vue-router";
+import { AuthData } from "../../types/auth";
+
+import { requiredInput } from "../../helpers/fieldValiadtonRules";
+
+const { isValid, validate } = useForm("form");
+const { replace } = useRouter();
+
+const formFields = reactive<AuthData>({
+  username: "",
+  password: ""
+});
+
+const gotoSignUp = () => {
+  replace({
+    name: "Login"
+  });
+};
+
+const submit = () => {
+  console.log(1);
+};
+</script>
 
 <style scoped></style>
