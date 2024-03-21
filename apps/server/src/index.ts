5c2bd254d715b593592a85ca47aff0de8706de1a
guzuro <guzurogmn@gmail.com>
2024-03-21 23:07:33 +0300

    server: fastify/cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,9 +2,15 @@ import { fastify, FastifyInstance } from "fastify";
 import autoLoad from "@fastify/autoload";
 import path from "path";
 import { fileURLToPath } from "url";
+import { connect } from 'mongoose'
+import cors from '@fastify/cors'
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const server: FastifyInstance = fastify();
+await server.register(cors, { 
+  origin: '*'
+})
+connect('mongodb://127.0.0.1:27017/maritime_dict_db')
 
 server.register(autoLoad, {
   dir: path.join(dirname, "routes"),
@@ -12,10 +18,6 @@ server.register(autoLoad, {
     prefix: "/api"
   }
 });
-
-// server.get('/ping', async (request, reply) => {
-//   return 'pong\n'
-// })
 
 server.listen({ port: 8080 }, () => {
   console.log("running");
